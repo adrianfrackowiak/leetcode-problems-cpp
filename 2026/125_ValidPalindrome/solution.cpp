ed22c059d0c047cadf462e26177af7298b2a909a
Adrian Frąckowiak <adrianfrackowiak@icloud.com>
2026-03-29 10:49:38 +0200

    75,125

---
+++ 2026/125_ValidPalindrome/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            char leftChar = tolower(s[left]);
+            char rightChar = tolower(s[right]);
+
+            if (!isalnum(leftChar)) {
+                left++;
+                continue;
+            }
+            if (!isalnum(rightChar)) {
+                right--;
+                continue;
+            }
+            if (leftChar != rightChar) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
